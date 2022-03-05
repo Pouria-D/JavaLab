31ef7b898a6207c4138b064acdeab253d149d2dd
Pouria-D <69520842+Pouria-D@users.noreply.github.com>
2022-03-05 20:15:55 +0330

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -64,7 +64,7 @@ public class Calculator {
             return false;
 
         // Check from 2 to n-1
-        for (int i = 2; i * i < n; i++)
+        for (int i = 2; i * i <= n; i++)
             if (n % i == 0)
                 return false;
 
