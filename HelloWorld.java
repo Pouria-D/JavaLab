aa1ca781e2792769a0d65e1494b7e7089d500b4d
Pouria-D <69520842+Pouria-D@users.noreply.github.com>
2022-05-10 22:20:06 +0430

    Delete HelloWorld.java

--- HelloWorld.java
+++
@@ -1,7 +0,0 @@
-
-// This program just print the "Hello World!" sentence
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
-}
