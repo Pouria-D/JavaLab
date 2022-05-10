b1e716484c30331779a9ca5e278560f223a9151e
Pouria-D <69520842+Pouria-D@users.noreply.github.com>
2022-05-10 22:22:52 +0430

    Add files via upload

---
+++ Lab1/src/Calculator.java
@@ -0,0 +1,85 @@
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class Calculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // set of operations
+        Set<String> operations = new HashSet<>(Arrays.asList("add","subtract","multiply","divide"));
+
+        // start the program and continue till user enter "quit"
+        while (true){
+            String operation = scanner.next();
+            if (operation.equals("quit"))
+                break;
+            else if (operation.equals("prime")){
+                int n = scanner.nextInt();
+                if (isPrime(n))
+                    System.out.println("true");
+                else
+                    System.out.println(false);
+                printPrime(n);
+            }else if (operations.contains(operation)){
+                // get inputs and calculate the result in output
+                double x= scanner.nextDouble();
+                double y= scanner.nextDouble();
+                double output = 0;
+                // choose the operation by switch case block
+                switch (operation){
+                    case "add":
+                        output = x + y;
+                        break;
+                    case "subtract":
+                        output = x - y;
+                        break;
+                    case "multiply":
+                        output = x * y;
+                        break;
+                    case "divide":
+                        //Handle Divide By Zero Exception
+                        try {
+                            output = x/y; // throw Exception
+                        }
+                        catch (ArithmeticException e) {
+                            // Exception handler
+                            System.out.println("Divided by zero operation cannot possible");
+                        }
+                        break;
+                    default:
+                        System.out.println("Invalid input!");
+                }
+                System.out.println(output);
+            } else
+                System.out.println("Invalid input!");
+        }
+    }
+    // function check whether a number is prime or not
+    static boolean isPrime(int n)
+    {
+        // Corner case
+        if (n <= 1)
+            return false;
+
+        // Check from 2 to n-1
+        for (int i = 2; i * i <= n; i++)
+            if (n % i == 0)
+                return false;
+
+        return true;
+    }
+
+    // Function to print primes
+    static void printPrime(int n)
+    {
+        for (int i = 2; i <= n; i++)
+        {
+            if (isPrime(i))
+                System.out.print(i + " ");
+        }
+        System.out.println();
+    }
+}
+
